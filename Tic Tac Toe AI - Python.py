156952524efe38a31c4e34f1ca3d3369c54cd3da
Aaron Escude <124310597+aescude@users.noreply.github.com>
2024-11-08 14:00:38 -0800

    Update Tic Tac Toe AI - Python.py
    Added additional comments

--- Tic Tac Toe AI - Python.py
+++ Tic Tac Toe AI - Python.py
@@ -152,18 +152,20 @@ class AaronMikeAI:
         #check if we have winning move
         for i in range(9):
             if game.is_valid_move(i):
-                game.board[i] = 'O'  # Assuming this AI plays 'O'
+                game.board[i] = 'O'  # Assuming the AI plays O
                 if game.check_win(game.board):
                     game.board[i] = ' '  # Reset for actual move
+                    #returns move we need to win
                     return i
                 game.board[i] = ' '  # Reset for next check
 
         #check if enemy has winning move
         for i in range(9):
             if game.is_valid_move(i):
-                game.board[i] = 'X'  # Check if opponent ('X') could win
+                game.board[i] = 'X'  # Check if opponent X could win
                 if game.check_win(game.board):
                     game.board[i] = ' '  # Reset for actual move
+                    #returns move we need to block opponent, basically their winning move
                     return i
                 game.board[i] = ' '  # Reset for next check
 
