cdbc9d651fa1c30b737915c3b49ad22a478f3253
Michaelis12379 <mestevez2026@smprep.org>
2024-11-25 20:12:34 -0800

    Update Tic Tac Toe AI - Python.py
    Added a comment to Class Player

--- Tic Tac Toe AI - Python.py
+++ Tic Tac Toe AI - Python.py
@@ -4,6 +4,7 @@
 # (c) 2024 Roland Labana
 import random
 
+# Defines the Player class, representing both human and AI players
 class Player:
     def __init__(self, symbol):
         self.symbol = symbol
